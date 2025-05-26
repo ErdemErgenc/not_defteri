eea042af77a4e49f2286556b6f5c2398f240169d
İbrahim Erdem Ergenç <erdem0165@gmail.com>
2025-05-26 14:35:36 +0300

    Initialize app with GetStorage and date formatting for Turkish locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:note_project1/note_home/note_home.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init(); // Bunu ekle!
   await initializeDateFormatting('tr_TR', null); // Türkçe locale'ı yükle
   runApp(const MyApp());
 }
