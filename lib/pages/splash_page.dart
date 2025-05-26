c8955595528d607b3c14476a0f5508fb80453bf8
İbrahim Erdem Ergenç <erdem0165@gmail.com>
2025-05-26 17:46:36 +0300

    Add animations package and implement splash screen animations for improved user experience

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:note_project1/note_home/note_home.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rive/rive.dart' as rive;
@@ -11,13 +12,74 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _opacityAnimation;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.offAll(() => NoteHome());
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+
+    // Scale animasyonunu önce yavaşça büyüt sonra sabit tut
+    _scaleAnimation = TweenSequence([
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.0,
+          end: 1.5,
+        ).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 70,
+      ),
+      TweenSequenceItem(tween: ConstantTween(1.5), weight: 30),
+    ]).animate(_controller);
+
+    // Opacity animasyonunu biraz geciktirerek yavaşça azalt
+    _opacityAnimation = TweenSequence([
+      TweenSequenceItem(
+        tween: ConstantTween(1.0),
+        weight: 70,
+      ), // 70% süre boyunca opak kal
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.0,
+          end: 0.0,
+        ).chain(CurveTween(curve: Curves.easeIn)),
+        weight: 30,
+      ),
+    ]).animate(_controller);
+
+    _controller.forward();
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.of(context).pushReplacement(
+          PageRouteBuilder(
+            transitionDuration: const Duration(milliseconds: 900),
+            pageBuilder: (context, animation, secondaryAnimation) => NoteHome(),
+            transitionsBuilder: (
+              context,
+              animation,
+              secondaryAnimation,
+              child,
+            ) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+          ),
+        );
+      }
     });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,25 +90,37 @@ class _SplashPageState extends State<SplashPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              width: 300.w,
-              height: 300.w,
-              child: rive.RiveAnimation.asset(
-                "lib/rive/notebook.riv",
-                fit: BoxFit.cover,
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Opacity(
+                  opacity: _opacityAnimation.value,
+                  child: Transform.scale(
+                    scale: _scaleAnimation.value,
+                    child: child,
+                  ),
+                );
+              },
+              child: SizedBox(
+                width: 300.w,
+                height: 300.w,
+                child: rive.RiveAnimation.asset(
+                  "lib/rive/notebook.riv",
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            SizedBox(height: 24.h),
+            SizedBox(height: 32.h),
             Text(
-              "Not Defterim",
-              style: TextStyle(
-                fontSize: 32.sp,
+              "Not Defterinize Hoşgeldiniz",
+              style: GoogleFonts.robotoSlab(
+                fontSize: 26.sp,
                 fontWeight: FontWeight.bold,
                 color: const Color(0xFF5E503F),
-                letterSpacing: 2,
+                letterSpacing: 1.2,
               ),
+              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 12.h),
           ],
         ),
       ),
